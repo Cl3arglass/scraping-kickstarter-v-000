9b74aaa3bcf1cfd455df06a40d7eb53bde6da286
Holden Altaffer <haltaffer@gmail.com>
2018-04-04 04:35:39 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,9 +11,12 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
-end
 
-create_project_hash
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
+end
